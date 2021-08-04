fe108b7c40e6397b16e787e7336497f52d50be9f
Dylan Cole <logboarddjc@gmail.com>
2021-08-04 19:50:11 -0400

    add changes from the acceptance criteria to the seachBooks.js file

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { ADD_BOOK } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
@@ -65,7 +67,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await useMutation(ADD_BOOK);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
