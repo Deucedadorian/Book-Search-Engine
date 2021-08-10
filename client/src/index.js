171e32aa3d1c75dbbc05bf9e5d026f07d136a7ad
Dylan Cole <logboarddjc@gmail.com>
2021-08-09 21:51:29 -0400

    change file names

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './app';
+import App from './app.js';
 
 ReactDOM.render(
   <React.StrictMode>
