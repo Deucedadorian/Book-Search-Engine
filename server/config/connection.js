472185fac32ff1df6acf6f9926972941d0eb41e6
Dylan Cole <logboarddjc@gmail.com>
2021-08-09 23:55:21 -0400

    add procfile

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,4 +7,14 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useFindAndModify: false,
 });
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+);
+
 module.exports = mongoose.connection;
